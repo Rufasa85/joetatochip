6c82d2cefb85d8bda94f0aab8ca5e562d1635e5f
Joe Rehfuss <joe.rehfuss@gmail.com>
2019-04-26 09:14:15 -0700

    Updates

---
+++ precache-manifest.4926b9b10938f016bdeff128d3a9bd15.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0848c5adbd25b4bf36b416804f30ff3",
+    "url": "/joetatochip/index.html"
+  },
+  {
+    "revision": "cf876f25c1c12535e93c",
+    "url": "/joetatochip/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/joetatochip/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "cf876f25c1c12535e93c",
+    "url": "/joetatochip/static/js/main.b9e2d134.chunk.js"
+  },
+  {
+    "revision": "a3029f7d3a5cb32a4e9d",
+    "url": "/joetatochip/static/js/runtime~main.5b7a6a21.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/joetatochip/static/media/logo.5d5d9eef.svg"
+  }
+]);
